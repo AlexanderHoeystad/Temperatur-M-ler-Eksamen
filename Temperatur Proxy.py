0b0c487cb8db699fea798540ab47e946d3f568c8
AlexanderHoeystad <Alek369@hotmail.com>
2024-05-13 13:24:39 +0200

    Proxy til api virker

--- Temperatur Proxy.py
+++ Temperatur Proxy.py
@@ -1,12 +1,17 @@
 from socket import *
+import requests
 
 serverPort = 10100
 serverSocket = socket(AF_INET, SOCK_DGRAM)
 
 serverAddress = ('', serverPort)
 
+api_address = "https://heatwaveprojekt.azurewebsites.net/api/Temp"
+headersArray = {'Content-Type': 'application/json'}
+
 serverSocket.bind(serverAddress)
 print("The server is ready")
 while True:
     message, clientAddress = serverSocket.recvfrom(2048)
     print("Received message:" + message.decode())
+    requests.post(api_address, data=message, headers=headersArray)
